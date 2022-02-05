f184465c2670c164e324684e7920ecbc6a6bc8dd
Erik Sundberg <erik.sundberg@postnord.com>
2022-02-05 17:58:38 +0100

    imports & cleanup

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,40 +1,23 @@
 import './App.css';
 import React from 'react';
+import { SearchBar } from '../SearchBar/SearchBar';
+import { SearchResults } from '../SearchResults/SearchResults'
+import { Playlist } from '../Playlist/Playlist';
 
 class App extends React.Component {
 
-  // function App() {
-  // return (
-  //   <div className="App">
-  //     <header className="App-header">
-  //       <img src={logo} className="App-logo" alt="logo" />
-  //       <p>
-  //         Edit <code>src/App.js</code> and save to reload.
-  //       </p>
-  //       <a
-  //         className="App-link"
-  //         href="https://reactjs.org"
-  //         target="_blank"
-  //         rel="noopener noreferrer"
-  //       >
-  //         Learn React
-  //       </a>
-  //     </header>
-  //   </div>
-  // );
   render() {
     return (<div>
-      <h1>Ja<span className="highlight">mmm</span>ing</h1>
+      <h1>Ja<span className="highlight">mm</span>able</h1>
       <div className="App">
-        {/* <!-- Add a SearchBar component --> */}
+        <SearchBar />
         <div className="App-playlist">
-          {/* <!-- Add a SearchResults component --> */}
-          {/* <!-- Add a Playlist component --> */}
+          <SearchResults />
+          <Playlist />
         </div>
       </div>
     </div>)
   }
 }
-// }
 
 export default App;

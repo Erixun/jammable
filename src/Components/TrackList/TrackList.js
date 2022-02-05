afc83a25c2f52e97f115d5ce34259f056842be98
Erik Sundberg <erik.sundberg@postnord.com>
2022-02-05 19:04:03 +0100

    map & render Tracks

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -1,12 +1,15 @@
 import React from "react";
 import './TrackList.css'
+import { Track } from "../Track/Track";
 
 export class TrackList extends React.Component {
 
   render() {
     return (
-      <div class="TrackList">
-        {/* <!-- You will add a map method that renders a set of Track components  --> */}
+      <div className="TrackList">
+        {this.props.tracks?.map(track => {
+          return (<Track name={track.name} artist={track.artist} album={track.album} key={track.id} />)
+        })}
       </div>
     )
   }

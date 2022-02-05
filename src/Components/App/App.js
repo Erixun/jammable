dfdf6fc15fd7e6329f62b97b3f8237fff374b532
Erik Sundberg <erik.sundberg@postnord.com>
2022-02-05 17:18:14 +0100

    move App to Components folder

---
+++ src/Components/App/App.js
@@ -0,0 +1,40 @@
+import './App.css';
+import React from 'react';
+
+class App extends React.Component {
+
+  // function App() {
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         Edit <code>src/App.js</code> and save to reload.
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a>
+  //     </header>
+  //   </div>
+  // );
+  render() {
+    return (<div>
+      <h1>Ja<span className="highlight">mmm</span>ing</h1>
+      <div className="App">
+        {/* <!-- Add a SearchBar component --> */}
+        <div className="App-playlist">
+          {/* <!-- Add a SearchResults component --> */}
+          {/* <!-- Add a Playlist component --> */}
+        </div>
+      </div>
+    </div>)
+  }
+}
+// }
+
+export default App;
